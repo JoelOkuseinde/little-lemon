b1b5cd814e8f13c16192355c63ceda804427ca1b
JoelOkuseinde <joelokuseinde456@gmail.com>
2023-08-16 13:18:48 +0100

    Third Commit

---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export class Nav extends Component {
+  render() {
+    return (
+      <>
+      <nav>
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+      </nav>
+      </>
+    )
+  }
+}
+
+export default Nav
